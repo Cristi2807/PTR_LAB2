cc9183cdeba700b2a49232fa77f3ff56e6b0e851
Cristi2807 <cristian.boris@isa.utm.md>
2023-03-13 23:00:24 +0200

    Implement W1 Main Task1

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule PtrLab2.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {PtrLab2, []}
     ]
   end
 
@@ -22,7 +23,9 @@ defmodule PtrLab2.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"
+      {:httpoison, "~> 2.0"},
+      {:poison, "~> 5.0"}
     ]
   end
 end
