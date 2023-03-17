25215dbeb9d83735a2b2aea4278a140e734362fb
Cristi2807 <cristian.boris@isa.utm.md>
2023-03-17 12:07:11 +0200

    Solve Minimal Task W3

--- lib/printer.ex
+++ lib/printer.ex
@@ -8,24 +8,46 @@ defmodule Printer do
 
   @impl true
   def init({id, min_time, max_time}) do
-    {:ok, {id, min_time, max_time}}
+    {:ok, bad_words_json} = File.read("./lib/bad_words.json")
+    {:ok, bad_words_dict} = Poison.decode(bad_words_json)
+
+    {:ok, {id, min_time, max_time, bad_words_dict}}
   end
 
   @impl true
-  def handle_info(:crash, {id, min_time, max_time}) do
+  def handle_info(:crash, {id, min_time, max_time, _bad_words_dict}) do
     IO.puts("#{id} received CRASH msg, GOING OFF")
     exit(:crash)
     {:noreply, {id, min_time, max_time}}
   end
 
   @impl true
-  def handle_info(json, {id, min_time, max_time}) do
+  def handle_info(json, {id, min_time, max_time, bad_words_dict}) do
     lambda = (max_time - min_time) / 2
 
     (min_time + round(Statistics.Distributions.Poisson.rand(lambda)))
     |> Process.sleep()
 
-    IO.puts("#{id}: #{json["message"]["tweet"]["text"]}")
-    {:noreply, {id, min_time, max_time}}
+    words = String.split(json["message"]["tweet"]["text"], " ", trim: true)
+
+    formattedWords =
+      Enum.map(words, fn word ->
+        case Enum.find(bad_words_dict["RECORDS"], fn %{"word" => value} ->
+               value == word |> String.downcase()
+             end) do
+          nil ->
+            word
+
+          badWord ->
+            badWord["word"]
+            |> String.split("", trim: true)
+            |> Enum.map(fn _ -> "*" end)
+            |> Enum.join()
+        end
+      end)
+      |> Enum.join(" ")
+
+    IO.puts("#{id}: #{formattedWords}")
+    {:noreply, {id, min_time, max_time, bad_words_dict}}
   end
 end
